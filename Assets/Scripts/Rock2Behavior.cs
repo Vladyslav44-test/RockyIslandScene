59629e87ccf3f0db3218e83cc773656923effe60
Vladislav44-test <elertvelert@gmail.com>
2026-08-24 17:26:48 +0300

    Додав скрипти для обертання майже всіх каменів

---
+++ Assets/Scripts/Rock2Behavior.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Rock2Behavior : MonoBehaviour
+{
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 1;
+    public float DegreesPerSecond = 90;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, new Vector3(0, 1, 0), DegreesPerSecond * Time.deltaTime);
+        transform.Rotate(axis, DegreesPerSecond * Time.deltaTime);
+    }
+}
